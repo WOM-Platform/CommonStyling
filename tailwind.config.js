c9980bb7f9c069dc37cf4fab82dbf266dd39b510
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-08-18 16:41:34 +0200

    Basic Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Alata', 'sans-serif'],
+      body: ['Poppins', 'sans-serif'],
+    },
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['26px', '32px'],
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        primary: '#2a69ff'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
